dbf2afe886a9ff0117f023d03c40d8f9622cfff1
mauricioaandrade <mauricioaandrade30@gmail.com>
2017-10-29 18:43:49 -0300

    aula 4 - adicao de DateHelper.js e model ListaNegociacoes.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,25 @@
 class NegociacaoController {
 
-    //now this set of code will do the query only once
+    //now this set of code will do the query into the DOM only once
     constructor() {
         let $=document.querySelector.bind(document);
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
+
     }
 
-    adiciona(event) {
-        event.preventDefault();
-
+    _criaNegociacao() {
+        
         //tres metodos de converter a data de string para formato correto
         //1 - como parametros de date (*mes 0-11)   
             // let data = new Date(...this._inputData // ... spread operator
@@ -21,22 +29,29 @@ class NegociacaoController {
         //2 - como array ano, mes, dia    
             //let data = new Date(this._inputData.value.split('-'));
         //3 - como uma string com  ',' em vez de '-'
-            let data = new Date(this._inputData.value.replace(/-/g, ','));
-        
-        console.log(data);
+            //let data = new Date(this._inputData.value.replace(/-/g, ','));
+            
+        //adicao do helper data
+        //let helper = new DateHelper();
+        //com a adicao de static nos metodos, a classe nao precisa (e nao pode) mais ser instaciada
 
-        // //this set of code will have to do a query each time the event is trigged - poor for performance
+        // //this set of code will have to do a query into de DOM each time the event is trigged - poor for performance
         // let $=document.querySelector.bind(document); //criando uma variável que se mantém conectada com o DOM através do "bind"
         // let inputData = $('#data');
         // let inputQuantidade =  $('#quantidade');
-        // let inputValor = $('#valor');
+        // let inputValor = $('#valor');        
 
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
+          return new Negociacao(
+                DateHelper.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.value
           );
-    
-          console.log(negociacao);
+    }            
+
+    adiciona(event) {
+        event.preventDefault();
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
     }
 }
